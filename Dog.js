ead88b6e6fe9c6db7f27e1625eaeea8351cd8106
Azathoth <letrunghoanglong2020@gmail.com>
2022-04-27 18:17:21 +0700

    Add .gitignore and install module

--- Dog.js
+++ Dog.js
@@ -1,4 +1,10 @@
-function function_name(argument) {
+var chalk = require('chalk');
+function Dog(name) {
 	// body...
+	this.name = name;
 	this.ear=[];
 }
+Dog.prototype.sayHi = function() {
+	console.log('Hi, I am a Dog. Hair is '+ chalk.red(this.name));
+};
+module.exports=Dog;
